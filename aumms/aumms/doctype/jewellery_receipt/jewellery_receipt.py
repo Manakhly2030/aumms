7957ab2495e436d771ace91519c463da3879b85b
Muhammed Sinan K T <91651425+MhmdSinanKT@users.noreply.github.com>
2024-08-21 14:03:54 +0530

    feat: purchase entry final changes

--- aumms/aumms/doctype/jewellery_receipt/jewellery_receipt.py
+++ aumms/aumms/doctype/jewellery_receipt/jewellery_receipt.py
@@ -14,12 +14,14 @@ class JewelleryReceipt(Document):
 			item_code_parts = [self.item_category, str(item_detail.gold_weight)]
 
 			if item_detail.has_stone:
-				if item_detail.single_stone:
-					item_code_parts.extend([item_detail.stone, str(item_detail.stone_weight)])
-				elif item_detail.multi_stone:
-					stones = item_detail.stones.split(',') if item_detail.stones else []
-					for stone in stones:
-						item_code_parts.append(stone)
+				# if item_detail.single_stone:
+				# 	item_code_parts.extend([item_detail.stone, str(item_detail.stone_weight)])
+				# elif item_detail.multi_stone:
+				# 	stones = item_detail.stones.split(',') if item_detail.stones else []
+				# 	for stone in stones:
+				# 		item_code_parts.append(stone)
+				for stone in self.item_wise_stone_details:
+					item_code_parts.append(stone.stone)
 
 			item_detail.item_code = ' '.join(item_code_parts)
 
@@ -56,25 +58,32 @@ class JewelleryReceipt(Document):
 			aumms_item.is_purchase_item = 1
 
 			if item_detail.has_stone:
-				if item_detail.single_stone:
-					aumms_item.append('stone_details', {
-						'stone_weight': item_detail.stone_weight,
-						'stone_charge': item_detail.stone_charge,
-						'item_name': item_detail.stone,
-						'stone_type': item_detail.stone,
+				# if item_detail.single_stone:
+				# 	aumms_item.append('stone_details', {
+				# 		'stone_weight': item_detail.stone_weight,
+				# 		'stone_charge': item_detail.stone_charge,
+				# 		'item_name': item_detail.stone,
+				# 		'stone_type': item_detail.stone,
+				# 	})
+				# else:
+				# 	stones = item_detail.stones.split(',') if item_detail.stones else []
+				# 	individual_stone_weight = item_detail.individual_stone_weight
+				# 	if individual_stone_weight:
+				# 		stone_weight = individual_stone_weight.split(',')
+				# 		for stone, weight in zip(stones, stone_weight):
+				# 			aumms_item.append('stone_details', {
+				# 				'stone_weight': float(weight),
+				# 				'stone_charge': item_detail.unit_stone_charge * float(weight),
+				# 				'item_name': stone,
+				# 				'stone_type': stone,
+				# 			})
+				for stone in self.item_wise_stone_details:
+					aumms_item.append("stone_details", {
+						"stone_weight": stone.stone_weight,
+						"stone_charge": stone.rate * stone.stone_weight,
+						"item_name": stone.stone,
+						"stone_type": stone.stone
 					})
-				else:
-					stones = item_detail.stones.split(',') if item_detail.stones else []
-					individual_stone_weight = item_detail.individual_stone_weight
-					if individual_stone_weight:
-						stone_weight = individual_stone_weight.split(',')
-						for stone, weight in zip(stones, stone_weight):
-							aumms_item.append('stone_details', {
-								'stone_weight': float(weight),
-								'stone_charge': item_detail.unit_stone_charge * float(weight),
-								'item_name': stone,
-								'stone_type': stone,
-							})
 
 			aumms_item.insert(ignore_permissions=True)
 			frappe.msgprint('AuMMS Item Created.', indicator="green", alert=1)
@@ -116,13 +125,13 @@ class JewelleryReceipt(Document):
 
 	def calculate_item_details(self):
 		for item_detail in self.get("item_details"):
-			if item_detail.single_stone:
-				if item_detail.stone_weight:
-					item_detail.net_weight = item_detail.gold_weight + item_detail.stone_weight
-					if item_detail.unit_stone_charge:
-						item_detail.stone_charge = item_detail.unit_stone_charge * item_detail.stone_weight
-			else:
-				item_detail.net_weight = item_detail.gold_weight
+			# if item_detail.single_stone:
+			# 	if item_detail.stone_weight:
+			# 		item_detail.net_weight = item_detail.gold_weight + item_detail.stone_weight
+			# 		if item_detail.unit_stone_charge:
+			# 			item_detail.stone_charge = item_detail.unit_stone_charge * item_detail.stone_weight
+			# else:
+			# 	item_detail.net_weight = item_detail.gold_weight
 			if self.board_rate:
 				if item_detail.has_stone:
 					item_detail.amount_without_making_charge = (item_detail.gold_weight * self.board_rate) + item_detail.stone_charge
