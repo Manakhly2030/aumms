0c9d195d5f2549121975def5e26d2d381118464e
Muhammed Sinan K T <91651425+MhmdSinanKT@users.noreply.github.com>
2024-08-30 15:13:14 +0530

    fix: hallmark return posting

--- aumms/aumms/doctype/hallmark_request/hallmark_request.py
+++ aumms/aumms/doctype/hallmark_request/hallmark_request.py
@@ -11,7 +11,7 @@ class HallmarkRequest(Document):
     def on_update_after_submit(self):
         if self.workflow_state == "Sent for Hallmarking":
             self.create_stock_entry()
-        elif self.workflow_state == "Hallmark Items Return":
+        elif self.workflow_state == "Items Hallmarked":
             self.create_stock_entry_to_finished_goods()
 
     def create_stock_entry(self):
@@ -36,24 +36,24 @@ class HallmarkRequest(Document):
             frappe.throw(_("Something went wrong while creating Stock Entry. Please check the error log."))
 
     def create_stock_entry_to_finished_goods(self):
-        try:
-            stock_entry = frappe.new_doc("Stock Entry")
-            stock_entry.stock_entry_type = "Manufacture"
-            stock_entry.from_warehouse = frappe.db.get_value("Warehouse", {"name": ["like", "%Work In Progress%"]}, "name")
-            stock_entry.to_warehouse = frappe.db.get_value("Warehouse", {"name": ["like", "%Finished Goods%"]}, "name")
+        stock_entry = frappe.new_doc("Stock Entry")
+        stock_entry.stock_entry_type = "Material Transfer"
+        from_warehouse = frappe.db.get_value("Warehouse", {"name": ["like", "%Work In Progress%"]}, "name")
+        to_warehouse = frappe.db.get_value("Warehouse", {"name": ["like", "%Finished Goods%"]}, "name")
 
-            for item in self.items:
-                stock_entry.append("items", {
-                    'item_code': item.item_code,
-                    'qty': 1,
-                    'basic_rate': item.amount,
-                    'uom': "Nos"
-                })
+        for item in self.items:
+            stock_entry.append("items", {
+                'item_code': item.item_code,
+                'qty': 1,
+                'uom': "Nos",
+                's_warehouse': from_warehouse,
+                't_warehouse': to_warehouse 
+            })
 
-            self.submit_stock_entry(stock_entry, "Stock Entry to Finished Goods")
-        except Exception as e:
-            frappe.log_error(f"Error Occurred in create_stock_entry_to_finished_goods: {e}")
-            frappe.throw(_("Something went wrong while creating Stock Entry to Finished Goods. Please check the error log."))
+        self.submit_stock_entry(stock_entry, "Stock Entry to Finished Goods")
+        
+        for item in stock_entry.items:
+            frappe.db.set_value("AuMMS Item", {"item_name", item.item_code}, "hallmarked", 1)
 
     def submit_stock_entry(self, stock_entry, entry_type):
         try:
